c653183b1f1f6450c1560f80304385e36a90c78f
lukas <lukasmwerk@gmail.com>
2024-10-14 11:43:18 -0400

    init

--- .github/workflows/generate_skills_map.py
+++ .github/workflows/generate_skills_map.py
@@ -9,14 +9,14 @@ def add_nodes_edges(dot, parent_name, path):
             dot.edge(parent_name, entry_path)  # Create an edge from parent to child
             add_nodes_edges(dot, entry_path, entry_path)  # Recursively add child nodes
 
-def generate_mind_map(repo_path):
+def generate_skills_map(repo_path):
     dot = Digraph(comment='Skills Map')
 
     dot.node(repo_path, os.path.basename(repo_path))
     add_nodes_edges(dot, repo_path, repo_path)
-    dot.render('skills-map', format='png', cleanup=True)
-    print("Mind map saved as 'skills-map.png'.")
+    dot.render('skills_map', format='png', cleanup=True)
+    print("Skills map saved as 'skills_map.png'.")
 
 if __name__ == "__main__":
-    repo_path = '.'
-    generate_mind_map(repo_path)
+    repo_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    generate_skills_map(repo_path)
